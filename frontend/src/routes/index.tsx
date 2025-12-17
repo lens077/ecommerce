6029e5d712601bc917b84a76b096904250767560
Sumery <xiconz@qq.com>
2025-12-17 14:16:11 +0800

    perf: perf get user profile

--- frontend/src/routes/index.tsx
+++ frontend/src/routes/index.tsx
@@ -1,5 +1,6 @@
 import { createFileRoute } from '@tanstack/react-router'
 import AppBar from "@/components/AppBar";
+import './index.css'
 
 export const Route = createFileRoute('/')({
     component: App,
