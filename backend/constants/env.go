31d526827b31ffb0fcbfc21985b2ed6d80e69ca7
Sumery <xiconz@qq.com>
2025-12-17 14:16:11 +0800

    perf: perf get user profile

---
+++ backend/constants/env.go
@@ -0,0 +1,9 @@
+package constants
+
+// 默认值
+const (
+	UserOwnerMetadataKey = "x-md-global-owner"
+	UserNameMetadataKey  = "x-md-global-name"
+	UserRoleMetadataKey  = "x-md-global-role"
+	UserIdMetadataKey    = "x-md-global-user-id"
+)
