e5c1a33714925b7ea7723657a98d4e916d3eaebc
lens <rcc@vip.qq.com>
2026-06-23 05:02:19 +0800

    chore(helm): update charts, templates and values

--- helm/helper.sh
+++ helm/helper.sh
@@ -16,3 +16,16 @@ helm registry login harbor.apikv.com:5443
 helm package .
 # 推送
 helm push *-*.tgz oci://harbor.apikv.com:5443/sumery
+
+# 创建secret
+kubectl create secret generic pg-ca-cert \
+  --namespace ecommerce \
+  --from-file=pg_ca.crt=./pg_ca_pem.crt
+
+# 安装
+helm upgrade --install ecommerce . \
+ -f values.yaml \
+ -n ecommerce \
+ --create-namespace
+
+helm uninstall ecommerce -n ecommerce
