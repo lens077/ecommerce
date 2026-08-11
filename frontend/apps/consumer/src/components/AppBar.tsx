9edf09de9e7a683558d845abebb0dff0c1e0c56e
lens <rcc@vip.qq.com>
2026-08-11 18:18:06 +0800

    chore(consumer): 删除搜索框从未触发的 width 过渡死代码

--- frontend/apps/consumer/src/components/AppBar.tsx
+++ frontend/apps/consumer/src/components/AppBar.tsx
@@ -98,7 +98,7 @@ const StyledInputBase = styled(InputBase)(({ theme }) => ({
     padding: theme.spacing(1, 1, 1, 0),
     // vertical padding + font size from searchIcon
     paddingLeft: `calc(1em + ${theme.spacing(4)})`,
-    transition: theme.transitions.create("width"),
+    // width 无任何状态触发变化(仅断点静态值),原 transition("width") 是从未播放的死代码,已删
     width: "100%",
     [theme.breakpoints.up("md")]: {
       width: "20ch",
